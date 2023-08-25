7b698bebc8a3edc6349fd2f1159b76f12e836049
Kumori <tauen8@gmail.com>
2023-08-24 21:35:17 -0500

    setting up the login js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,4 +5,11 @@ router.get('/', async (req, res) => {
   res.render('homepage');
 });
 
+router.get ('/dashboard', async (req, res) => {
+  res.render('dashboard');
+})
+
+router.get ('/login', async (req, res) => {
+  res.render('login');
+})
 module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,52 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+      alert("Login success!")
+    } else {
+      alert("Incorrect email or password. Please try again");
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/login")
+      alert("Success! Please login with your new profile!");
+    } else {
+      alert("Sign up failed. Please make sure email and password are unique.");
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
